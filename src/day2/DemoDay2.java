97e6679a06b9a18811cae5981384c6c5986b1ce3
salsh <salsh@Sally.lan>
2025-01-31 18:19:04 -0500

    adding last name

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,6 +4,9 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 		String name = "Sally";
 		System.out.println(name);
+		
+		String lastName = "Alshakkakee";
+		System.out.println(lastName);
 	}
 
 }
